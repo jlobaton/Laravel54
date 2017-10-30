35300ef0b0c5d9b79eaf104d78be9e154014e565
PC- Jesus Lobaton <jesuslobaton@gmail.com>
2017-10-30 00:57:05 +0000

    configuracion inicial

--- config/app.php
+++ config/app.php
@@ -170,6 +170,7 @@ return [
 
         Collective\Html\HtmlServiceProvider::class,
         Laracasts\Flash\FlashServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -230,7 +231,8 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 
         'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,        
+        'Html' => Collective\Html\HtmlFacade::class, 
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,       
 
     ],
 
